c80fbb32ea22e6ec738de6cbb0e567ffe92a3e80
mutouyun <orz@orzz.org>
2018-12-17 22:58:01 +0800

    remove test_prod_cons<1, 8>()

--- test/test_ipc.cpp
+++ test/test_ipc.cpp
@@ -383,7 +383,6 @@ struct test_performance<1, 1> {
 
 void Unit::test_channel_performance() {
     test_prod_cons<1, 1>();
-    test_prod_cons<1, 8>();
 
     test_performance<1 , 10>::start();
     test_performance<10, 1 >::start();
