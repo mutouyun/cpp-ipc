8832877c6de9dd0279a3660537a346a77dab8ada
mutouyun <orz@orzz.org>
2019-01-15 21:37:56 +0800

    update

--- src/platform/waiter.h
+++ src/platform/waiter.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #if defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || \
     defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || \
     defined(WINCE) || defined(_WIN32_WCE)
