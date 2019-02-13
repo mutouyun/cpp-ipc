e88a4cf865b32631620a5062765da8a94c692e28
mutouyun <mark.lonr@tom.com>
2019-02-13 15:48:55 +0800

    add demo-chat

---
+++ demo/chat/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include "ipc.h"
+
+namespace {
+
+char name__[] = "ipc-chat";
+char quit__[] = "q";
+
+} // namespace
+
+int main() {
+    std::string buf;
+    ipc::channel cc { name__ };
+
+    std::thread r {[] {
+        ipc::channel cc { name__ };
+        while (1) {
+            auto buf = cc.recv();
+            if (buf.empty()) continue;
+            std::string dat { static_cast<char const *>(buf.data()), buf.size() - 1 };
+            if (dat == quit__) return;
+            std::cout << dat << std::endl;
+        }
+    }};
+
+    while (1) {
+        std::cin >> buf;
+        cc.send(buf);
+        if (buf == quit__) break;
+    }
+
+    r.join();
+    return 0;
+}
