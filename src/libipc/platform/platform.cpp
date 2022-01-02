1f65fc98325f1c213bf85b9a4170a311e808e54d
mutouyun <orz@orzz.org>
2022-01-02 17:56:05 +0800

    compile error

--- src/libipc/platform/platform.cpp
+++ src/libipc/platform/platform.cpp
@@ -3,7 +3,7 @@
 #if defined(IPC_OS_WINDOWS_)
 #include "libipc/platform/win/shm_win.cpp"
 #elif defined(IPC_OS_LINUX_) || defined(IPC_OS_QNX_)
-#include "libipc/platform/linux/shm_posix.cpp"
+#include "libipc/platform/posix/shm_posix.cpp"
 #else/*IPC_OS*/
 #   error "Unsupported platform."
 #endif
