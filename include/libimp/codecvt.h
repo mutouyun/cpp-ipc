8231d6f89e6165c7594b692478c9c2a3e18cfd1e
mutouyun <orz@orzz.org>
2022-08-13 17:52:00 +0800

    add: [codecvt] support for char8_t, char16_t, and char32_t

--- include/libimp/codecvt.h
+++ include/libimp/codecvt.h
@@ -15,12 +15,24 @@
 LIBIMP_NAMESPACE_BEG_
 
 /**
- * @brief The transform between local-character-set(UTF-8/GBK/...) and UTF-16/32
+ * @brief The transform between UTF-8/16/32
  * 
  * @param des  The target string pointer can be nullptr
  * @param dlen The target string length can be 0
  */
 template <typename CharT, typename CharU>
 LIBIMP_EXPORT std::size_t cvt_cstr(CharT const *src, std::size_t slen, CharU *des, std::size_t dlen) noexcept;
+
+template <typename CharT, typename TraitsT, typename AllocT, 
+          typename CharU, typename TraitsU, typename AllocU>
+void cvt_sstr(std::basic_string<CharT, TraitsT, AllocT> const &src, std::basic_string<CharU, TraitsU, AllocU> &des) {
+  std::size_t dlen = cvt_cstr(src.c_str(), src.size(), (CharU *)nullptr, 0);
+  if (dlen == 0) {
+    des.clear();
+    return;
+  }
+  des.resize(dlen);
+  cvt_cstr(src.c_str(), src.size(), &des[0], des.size());
+}
 
 LIBIMP_NAMESPACE_END_
