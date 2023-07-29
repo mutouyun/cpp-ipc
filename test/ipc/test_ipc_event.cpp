7ec99c33940905805307d7e5807c2c900ac90e24
mutouyun <orz@orzz.org>
2023-07-29 13:27:22 +0800

    add some simple event tests

--- test/ipc/test_ipc_event.cpp
+++ test/ipc/test_ipc_event.cpp
@@ -4,4 +4,16 @@
 #include <libipc/event.h>
 
 TEST(event, open_close) {
+  auto evt = ipc::evt_open("test");
+  ASSERT_TRUE(evt);
+  ASSERT_EQ(ipc::evt_name(*evt), "test");
+  ASSERT_TRUE(ipc::evt_close(*evt));
+}
+
+TEST(event, set_wait) {
+  auto evt = ipc::evt_open("test");
+  ASSERT_TRUE(evt);
+  ASSERT_TRUE(ipc::evt_set(*evt));
+  ASSERT_TRUE(ipc::evt_wait(*evt, 0));
+  ASSERT_TRUE(ipc::evt_close(*evt));
 }
