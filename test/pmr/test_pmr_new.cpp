a5456d925848dc77c8e737208f7a7b14a1e561a1
mutouyun <orz@orzz.org>
2024-03-09 21:04:21 +0800

    Refactor regular_sizeof ut

--- test/pmr/test_pmr_new.cpp
+++ test/pmr/test_pmr_new.cpp
@@ -10,13 +10,13 @@
 #include "libpmr/new.h"
 
 TEST(pmr_new, regular_sizeof) {
-  ASSERT_EQ(pmr::regular_sizeof<std::int8_t >(), pmr::regular_head_size + 8);
-  ASSERT_EQ(pmr::regular_sizeof<std::int16_t>(), pmr::regular_head_size + 8);
-  ASSERT_EQ(pmr::regular_sizeof<std::int32_t>(), pmr::regular_head_size + 8);
-  ASSERT_EQ(pmr::regular_sizeof<std::int64_t>(), pmr::regular_head_size + 8);
+  ASSERT_EQ(pmr::regular_sizeof<std::int8_t >(), pmr::regular_head_size + alignof(std::max_align_t));
+  ASSERT_EQ(pmr::regular_sizeof<std::int16_t>(), pmr::regular_head_size + alignof(std::max_align_t));
+  ASSERT_EQ(pmr::regular_sizeof<std::int32_t>(), pmr::regular_head_size + alignof(std::max_align_t));
+  ASSERT_EQ(pmr::regular_sizeof<std::int64_t>(), pmr::regular_head_size + alignof(std::max_align_t));
 
-  ASSERT_EQ((pmr::regular_sizeof<std::array<char, 10    >>()), ::LIBIMP::round_up<std::size_t>(pmr::regular_head_size + 10   , 8));
-  ASSERT_EQ((pmr::regular_sizeof<std::array<char, 100   >>()), ::LIBIMP::round_up<std::size_t>(pmr::regular_head_size + 100  , 8));
+  ASSERT_EQ((pmr::regular_sizeof<std::array<char, 10    >>()), ::LIBIMP::round_up<std::size_t>(pmr::regular_head_size + 10   , alignof(std::max_align_t)));
+  ASSERT_EQ((pmr::regular_sizeof<std::array<char, 100   >>()), ::LIBIMP::round_up<std::size_t>(pmr::regular_head_size + 100  , alignof(std::max_align_t)));
   ASSERT_EQ((pmr::regular_sizeof<std::array<char, 1000  >>()), ::LIBIMP::round_up<std::size_t>(pmr::regular_head_size + 1000 , 128));
   ASSERT_EQ((pmr::regular_sizeof<std::array<char, 10000 >>()), ::LIBIMP::round_up<std::size_t>(pmr::regular_head_size + 10000, 8192));
   ASSERT_EQ((pmr::regular_sizeof<std::array<char, 100000>>()), (std::numeric_limits<std::size_t>::max)());
