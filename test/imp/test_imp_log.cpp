6ce508bac92d2ca38c76c04429889e2c7df25e08
mutouyun <orz@orzz.org>
2025-01-07 20:09:24 +0800

    Add `log`

---
+++ test/imp/test_imp_log.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <string>
+
+#include "test.h"
+
+#include "libipc/imp/log.h"
+
+TEST(log, logger) {
+  {
+    LIBIPC_LOG();
+    log.info("hello");
+  }
+  {
+    LIBIPC_LOG();
+    log.info("hello 2");
+  }
+  {
+    LIBIPC_LOG();
+    log.info("hello ", 3);
+  }
+  SUCCEED();
+}
+
+TEST(log, custom) {
+  struct log {
+    std::string i;
+    std::string e;
+  } ll_data;
+  auto ll = [&ll_data](auto &&ctx) {
+    auto s = imp::fmt(ctx.params);
+    if (ctx.level == imp::log::level::error) ll_data.e += s + " ";
+    else
+    if (ctx.level == imp::log::level::info ) ll_data.i += s + " ";
+  };
+
+  LIBIPC_LOG(ll);
+
+  log.info ("hello", " world");
+  log.error("failed", ":");
+  log.info ("log", '-', "pt");
+  log.error("whatever");
+
+  EXPECT_EQ(ll_data.i, "hello world log-pt ");
+  EXPECT_EQ(ll_data.e, "failed: whatever ");
+}
