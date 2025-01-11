99272a452a3f25e67b11b0ac8ba1d22443b14ef7
mutouyun <orz@orzz.org>
2025-01-11 18:45:25 +0800

    Simplify verify_args function to fix error C3249

--- src/libipc/mem/verify_args.h
+++ src/libipc/mem/verify_args.h
@@ -9,13 +9,7 @@ namespace mem {
  * \brief Check that bytes is not 0 and that the alignment is a power of two.
  */
 inline constexpr bool verify_args(std::size_t bytes, std::size_t alignment) noexcept {
-  if (bytes == 0) {
-    return false;
-  }
-  if ((alignment == 0) || (alignment & (alignment - 1)) != 0) {
-    return false;
-  }
-  return true;
+  return (bytes > 0) && (alignment > 0) && ((alignment & (alignment - 1)) == 0);
 }
 
 } // namespace mem
