716992cade5c1a7ca3dee433d916e6740c83ed24
mutouyun <orz@orzz.org>
2025-02-13 15:28:14 +0800

    Reimplement the allocator required for the container type with `$new`

--- test/mem/test_mem_bytes_allocator.cpp
+++ test/mem/test_mem_bytes_allocator.cpp
@@ -8,15 +8,15 @@
 # include <memory_resource>
 #endif
 
-#include "libipc/mem/polymorphic_allocator.h"
+#include "libipc/mem/bytes_allocator.h"
 #include "libipc/mem/memory_resource.h"
 
-TEST(polymorphic_allocator, ctor) {
+TEST(bytes_allocator, ctor) {
   ipc::mem::bytes_allocator alc;
   SUCCEED();
 }
 
-TEST(polymorphic_allocator, ctor_value_initialization) {
+TEST(bytes_allocator, ctor_value_initialization) {
   ipc::mem::bytes_allocator alc{};
   auto p = alc.allocate(128);
   EXPECT_NE(p, nullptr);
@@ -37,14 +37,14 @@ public:
 
 } // namespace
 
-TEST(polymorphic_allocator, memory_resource_traits) {
+TEST(bytes_allocator, memory_resource_traits) {
   EXPECT_FALSE(ipc::mem::has_allocate<void>::value);
   EXPECT_FALSE(ipc::mem::has_allocate<int>::value);
   EXPECT_FALSE(ipc::mem::has_allocate<std::vector<int>>::value);
   EXPECT_FALSE(ipc::mem::has_allocate<std::allocator<int>>::value);
 #if defined(LIBIMP_CPP_17) && defined(__cpp_lib_memory_resource)
   EXPECT_TRUE (ipc::mem::has_allocate<std::ipc::mem::memory_resource>::value);
-  EXPECT_TRUE (ipc::mem::has_allocate<std::ipc::mem::polymorphic_allocator<int>>::value);
+  EXPECT_TRUE (ipc::mem::has_allocate<std::ipc::mem::container_allocator<int>>::value);
 #endif
 
   EXPECT_FALSE(ipc::mem::has_deallocate<void>::value);
@@ -53,11 +53,11 @@ TEST(polymorphic_allocator, memory_resource_traits) {
   EXPECT_FALSE(ipc::mem::has_deallocate<std::allocator<int>>::value);
 #if defined(LIBIMP_CPP_17) && defined(__cpp_lib_memory_resource)
   EXPECT_TRUE (ipc::mem::has_deallocate<std::ipc::mem::memory_resource>::value);
-  EXPECT_FALSE(ipc::mem::has_deallocate<std::ipc::mem::polymorphic_allocator<int>>::value);
+  EXPECT_FALSE(ipc::mem::has_deallocate<std::ipc::mem::container_allocator<int>>::value);
 #endif
 }
 
-TEST(polymorphic_allocator, ctor_copy_move) {
+TEST(bytes_allocator, ctor_copy_move) {
   ipc::mem::new_delete_resource mem_res;
   dummy_resource dummy_res;
   ipc::mem::bytes_allocator alc1{&mem_res}, alc2{&dummy_res};
@@ -79,7 +79,7 @@ TEST(polymorphic_allocator, ctor_copy_move) {
   ASSERT_NO_THROW(alc5.deallocate(p, 128));
 }
 
-TEST(polymorphic_allocator, swap) {
+TEST(bytes_allocator, swap) {
   ipc::mem::new_delete_resource mem_res;
   dummy_resource dummy_res;
   ipc::mem::bytes_allocator alc1{&mem_res}, alc2{&dummy_res};
@@ -90,7 +90,7 @@ TEST(polymorphic_allocator, swap) {
   ASSERT_EQ(alc1.allocate(128), nullptr);
 }
 
-TEST(polymorphic_allocator, invalid_alloc_free) {
+TEST(bytes_allocator, invalid_alloc_free) {
   ipc::mem::bytes_allocator alc1;
   EXPECT_EQ(alc1.allocate(0), nullptr);
   EXPECT_NO_THROW(alc1.deallocate(nullptr, 128));
@@ -98,6 +98,6 @@ TEST(polymorphic_allocator, invalid_alloc_free) {
   EXPECT_NO_THROW(alc1.deallocate(&alc1, 0));
 }
 
-TEST(polymorphic_allocator, sizeof) {
+TEST(bytes_allocator, sizeof) {
   EXPECT_EQ(sizeof(ipc::mem::bytes_allocator), sizeof(void *) * 2);
 }
